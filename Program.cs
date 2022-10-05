a01e4cc77104750e018663881b605957556ae68a
panar <pan.arno@gmail.com>
2022-10-05 00:33:09 -0700

    so difficult

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using HealthAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<HealthContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+  options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
+
+// Add Cors
+builder.Services.AddCors(o => o.AddPolicy("HealthPolicy", builder => {
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+
+var app = builder.Build(); //================================
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseCors("HealthPolicy"); 
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// apply outstanding migrations
+// dotnet ef database update
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<HealthContext>();    
+    context.Database.Migrate();
+}
+
+app.Run();
